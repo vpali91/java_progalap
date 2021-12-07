b9b5ee4ec8ef4e3a2d01d0ae7782300effddd691
vpali91 <44944542+vpali91@users.noreply.github.com>
2021-12-07 10:09:08 +0100

    Create Kutya.java
    Gyermek osztály

---
+++ documentation/OOP/inheritance_intro/Allatok/Kutya.java
@@ -0,0 +1,12 @@
+package allatok;
+
+public class Kutya extends Emlos {
+
+    public Kutya(int eletkor, int testsuly) {
+        super(eletkor, testsuly);
+    }
+
+    public String Ugat() {
+        return "Vau";
+    }
+}
