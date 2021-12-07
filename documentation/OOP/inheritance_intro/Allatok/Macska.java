6d7a332b109705eab176af90b3b32c8ab9a10b58
vpali91 <44944542+vpali91@users.noreply.github.com>
2021-12-07 10:09:38 +0100

    Create Macska.java
    Gyermekosztály

---
+++ documentation/OOP/inheritance_intro/Allatok/Macska.java
@@ -0,0 +1,12 @@
+package allatok;
+
+public class Macska extends Emlos {
+
+    public Macska(int eletkor, int testsuly) {
+        super(eletkor, testsuly);
+    }
+
+    public String Karmol() {
+        return "Megkarmolt";
+    }
+}
